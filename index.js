d585913d7491058f01fd0eff9d26a778d47f9a7e
Sarah Schaller <sarahcschaller@gmail.com>
2018-06-10 18:21:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const driver = 'bob';
 
-console.log('this code was called')
-console.log(driver)
-console.log(driver, 'is the driver variable name')
+console.log('this code was called');
+console.log(driver);
+console.log(driver, 'is the driver variable name');
